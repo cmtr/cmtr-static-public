59d49800c654e754462943311395b26820edc3b9
Harald Blikø <haraldbliko@cmtr.io>
2023-01-12 22:40:29 +0100

    automating render

--- content/builder/index.js
+++ content/builder/index.js
@@ -1,4 +1,7 @@
+const { readFileSync, writeFileSync } = require("fs");
+const yaml = require("js-yaml");
 const buildContext = require("./build");
+const Render = require("./render");
 
 const rootDirectory = __dirname + "/../"
 const contentPath = rootDirectory + "content/index.yml"
@@ -11,4 +14,8 @@ const renderConfigOutputPath = rootDirectory + "./output/page.yml";
 
 const renderConfig = buildContext(renderConfigPath, renderConfigOutputPath, { rootDirectory })
 
-console.log(renderConfig);
+const render = Render(rootDirectory);
+
+const rendered = renderConfig.render.map(render);
+
+writeFileSync(rootDirectory + "./output/rendered.json", JSON.stringify(rendered))
--- content/builder/render.js
+++ content/builder/render.js
@@ -3,21 +3,9 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const yaml = require("js-yaml");
 
-const rootDirectory = __dirname + "/../";
-const outputDirectory = rootDirectory + "./output"
-const path = rootDirectory + "./test-page.yml";
-const payload = yaml.load(readFileSync(path, { encoding: "utf-8" }));
-
-
-const pageRenderConfig = payload.render[0];
-
 const DEFAULT_OPTIONS = {
 	encoding: "utf-8"
 }
-
-// Need to draw up the process as a UML Diagram 
-
-const ejsPredicate = (obj) => obj.template.type && obj.template.type === "ejs"
 
 const ejsRender = (rootDirectory, defaultOptions={}) => 
 	(obj, overrideOptions={}) => {
@@ -35,9 +23,36 @@ const ejsRender = (rootDirectory, defaultOptions={}) =>
 		return template(obj.context);
 	}
 
-const ejsCase = {
-	predicate: ejsPredicate,
-	action: ejsRender
-}
 
-console.log(ejsRender(rootDirectory, DEFAULT_OPTIONS)(pageRenderConfig))
+module.exports = (rootDirectory, overrideOptions={}) => (obj) => {
+	const userOptions = obj.template.options || {}
+	const options = {
+		...DEFAULT_OPTIONS,
+		...overrideOptions,
+		...userOptions
+	}
+
+	const templatePath = rootDirectory + obj.template.path;
+	
+	let output;
+	let error;
+	switch (obj.template.type) {
+		case "EJS":
+			ejs.renderFile(templatePath, obj.context, options, (err, str) => {
+				output = str;
+				error = err;
+				console.log(err);
+			});
+			break;
+		default:
+			console.log("Throw some error here");
+			console.log(obj);
+	}
+
+	return {
+		...obj,
+		render: {
+			output, error
+		}
+	};
+}
