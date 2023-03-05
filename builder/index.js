4bc08fe76164dc9b6a9400acec52a75ab9bd0b3c
Harald Blikø <haraldbliko@cmtr.io>
2023-03-05 18:55:26 +0100

    starting with new structure

--- builder/index.js
+++ builder/index.js
@@ -6,16 +6,3 @@ const Render = require("./render");
 const rootDirectory = __dirname + "/../"
 const contentPath = rootDirectory + "content/index.yml"
 const contextPath = rootDirectory + "./output/context.yml";
-
-buildContext(contentPath, contextPath, { rootDirectory: rootDirectory + "./content/"});
-
-const renderConfigPath = rootDirectory + "./page.yml"
-const renderConfigOutputPath = rootDirectory + "./output/page.yml";
-
-const renderConfig = buildContext(renderConfigPath, renderConfigOutputPath, { rootDirectory })
-
-const render = Render(rootDirectory);
-
-const rendered = renderConfig.render.map(render);
-
-writeFileSync(rootDirectory + "./output/rendered.json", JSON.stringify(rendered))
