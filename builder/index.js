8be59e74138f80496fd23e8fc778a5ea945ee191
Harald Blikø <haraldbliko@cmtr.io>
2023-03-05 21:08:23 +0100

    render of about page successfull

--- builder/index.js
+++ builder/index.js
@@ -1,13 +1,47 @@
 const { readFileSync, writeFileSync } = require("fs");
 const path = require("path");
 const yaml = require("js-yaml");
+const ejs = require("ejs");
+const marked = require("marked");
 const util = require("yaml-util");
 
 const rootDirectory = path.resolve(__dirname, "..");
 const contentPath = path.resolve(rootDirectory, "content/index.yml");
-const pagePath = path.resolve(rootDirectory, "site/index.yml");
+const sitePath = path.resolve(rootDirectory, "site/index.yml");
 
-
+// Build content
 const content = util.importFile()(contentPath)
+const contentOutputPath = path.resolve(rootDirectory, "output/content.yml");
+writeFileSync(contentOutputPath, yaml.dump(content), { encoding: "utf8"});
 
-console.log(content);
+// Build page
+const site = util.ref(util.importFile()(sitePath));
+const siteOutputPath = path.resolve(rootDirectory, "output/site.yml");
+
+// Render
+
+site.render.forEach((page) => {
+	try {
+		const templatePath = path.resolve(rootDirectory, page.template.path);
+		writeFileSync(path.resolve(rootDirectory, "output/pageContext.yml"), yaml.dump(page.context), { encoding: "utf8"});
+		
+		const data = {
+			 ...content, // Global Context
+			 ...page.context, // Page Specific Context
+			 // Utility functions 
+			 // Note: It should not be possible to override utility function
+			 marked: marked.parse 
+		};
+
+		ejs.renderFile(templatePath, data, (err, html) => {
+			if (err) throw err;
+			const outputPath = path.resolve(rootDirectory, page.output.path);
+			writeFileSync(outputPath, html, { encoding: "utf8"});
+		});
+
+		console.log("success");
+	} catch (error) {
+		console.log(`Page "${page.id}" failed to render`);
+		console.trace(error);
+	}
+});
